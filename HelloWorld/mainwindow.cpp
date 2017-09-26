f89815e96812387d4a2d0b0155e8683e65348a20
Nikolai_Laptop <Nikolai.stecker@gmx.de>
2017-09-26 19:02:37 +0200

    Hello world mit opencv erweitert

--- HelloWorld/mainwindow.cpp
+++ HelloWorld/mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <opencv2/opencv.hpp>
+#include "cvmattoqimage.h"
+
+using namespace cv;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
